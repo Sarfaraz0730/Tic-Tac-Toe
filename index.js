36e84fca0b588cdf74f6d9c971edacbaa27a61d9
Sarfaraz0730 <sarfarazpycoder@gmail.com>
2023-10-28 17:19:08 +0530

    callbacks

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
-var x =1;
+setTimeout(function(){
 
-a()
-b()
-console.log(x);
+        console.log(`timer`)
 
-function a(){
-    var x =10;
-    console.log(x);
+},5000)
+
+function x(y){
+    console.log("x")
+    y()
 }
 
-function b(){
-    var x =100;
-    console.log(x);
-}
-// console.log("Before expression anonymousFunctioin : ",anonymousFunctioin(2,3))
-const anonymousFunctioin =(a,b)=> a+b;
-console.log("after expression anonymousFunctioin : ",anonymousFunctioin(2,3))
-//curring function
-function add(a){
-  return function(b){
- return a+b
-  }
-}
-const firstArg = add(5)
-
-console.log(firstArg(10))
+x(function y(){
+  console.log("y")
+})
