61b8f89298603e44c215f34473ab5d4638ef6751
Sarfaraz0730 <sarfarazpycoder@gmail.com>
2023-11-02 10:18:13 +0530

    game is underprocessing

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-console.log("welcome to tic tac toe")
-
 let turnAudio =new Audio("turn.mp3")
-// let turnAudio =new Audio("turn.pm3")
-// let gameover =new Audio("gameovermp3")
-// console.log("turnAudio", turnAudio)
-console.log("turnAudio", turnAudio)
-
-// console.log("gameover", gameover)
-
 let turn ="X"
+let isGameover =false
 
 // function to change the turn 
 const changeTurn=()=>{
@@ -17,25 +9,68 @@ const changeTurn=()=>{
 // function to check value 
 
 const checkWin =()=>{
+    var boxText = document.getElementsByClassName("boxtext")
+let wins = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,5],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+wins.forEach(e=>{
+    if((boxText[e[0]].innerText===boxText[e[1]].innerText ) && (boxText[e[2]].innerText===boxText[e[1]].innerText) && (boxText[e[0]].innerText !=="") ){
+     document.querySelector(".info").innerText=boxText[e[0]].innerText + ` :  Won`;
+     isGameover = true;
+     document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width="230px"
+    }
+})
+
 
 }
 
+const matchTied = ()=>{
+    if(!checkWin()){
+        console.log("no one win yet")
+    }else{
+        console.log("game is still on")
+    }
+    
+}
 
 // Game logic 
 let boxes = document.getElementsByClassName("box");
 
 Array.from(boxes).forEach(Element=>{
-    console.log("boxes ",boxes)
+
     let boxtext = Element.querySelector(".boxtext");
     Element.addEventListener("click", ()=>{
         if(boxtext.innerText ===""){
         boxtext.innerText= turn;
-         changeTurn();
+        turn = changeTurn();
          turnAudio.play()
          checkWin()
-         document.getElementsByClassName("info")[0].innerText ="Turn for : " +turn
+         matchTied()
+         if(!isGameover){
+            document.getElementsByClassName("info")[0].innerText ="Turn for : " +turn
+         }
         }
     })
-console.log("boxtext")
+
+
+})
+
+reset.addEventListener("click", (e)=>{
+    let boxText = document.querySelectorAll(".boxtext");
+    Array.from(boxText).forEach(element=>{
+        element.innerText=""
+    })
+    turn ="X";
+    isGameover=false;
+    document.getElementsByClassName("info")[0].innerText ="Turn for : " +turn;
+    document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width="0px"
+
 
 })
